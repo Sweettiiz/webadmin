acd3846dfcf3d50904adfe5f1850c1219ccd6721
prakasitfrank0341 <53758396+prakasitfrank0341@users.noreply.github.com>
2024-05-22 07:00:39 +0700

    add contents in UserToken.vue

---
+++ src/pages/user-token/components/ProjectStatusBadge.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { PropType } from 'vue'
+import { Project } from '../types'
+
+defineProps({
+  status: {
+    type: String as PropType<Project['status']>,
+    required: true,
+  },
+})
+
+const badgeColorMap: Record<Project['status'], string> = {
+  active: 'success',
+  inactive: 'danger',
+}
+</script>
+
+<template>
+  <VaBadge square :color="badgeColorMap[$props.status]" :text="$props.status.toUpperCase()" />
+</template>
